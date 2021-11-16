04c18e774fa8a9599ceb2f3ceb3c3d5f3d92c9c4
Jay Nguyen <jay@Jays-iMac.local>
2021-11-16 15:10:32 -0800

    Completed Backend

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 const vinylsController = require('./controllers/vinylsController');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+
+app.use(cors());
 
 app.use(morgan('combined'));
 
